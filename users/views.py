5038ebcf2fb461568d94e527635764207df702bd
Kirill <kirill.rylskij96@gmail.com>
2026-07-11 13:53:25 +0300

    удаление лишнего импорта и изменение формата файла

--- users/views.py
+++ users/views.py
@@ -1,7 +1,5 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
 
 from .models import User
 from .serializers import UserRegisterSerializer, UserSerializer
